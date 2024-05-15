4882debe2f80aff5cff9bf576834dda01869e92a
guiladg <guiladg@gmail.com>
2024-05-15 15:17:03 -0300

    First final version

--- src/App.tsx
+++ src/App.tsx
@@ -10,6 +10,9 @@ import video from './assets/video.mp4';
 import { texts } from './texts';
 import { setAsyncTimeout } from './setAsyncTimeOut';
 
+const cardImage = [card1, card2, card3, card4, card5];
+const cardImageBig = [card1, card2, card3, card4, card5];
+
 type Page = 1 | 2 | 3;
 
 type CardOption = 1 | 2 | 3 | 4 | 5;
@@ -31,7 +34,7 @@ function App() {
 	const selected1 = selection[0] === undefined ? undefined : cards[selection[0] as number];
 	const selected2 = selection[1] === undefined ? undefined : cards[selection[1] as number];
 	const equalSelection = selected1 != undefined && selected1 === selected2;
-	console.log(selected1, selected2, cards);
+	console.log(cards);
 
 	useEffect(() => {
 		if (!selection.length) {
@@ -91,60 +94,70 @@ function App() {
 	};
 
 	return (
-		<>
-			{page === 2 ? (
-				<div className="game">
-					<div className="header">
-						<h2>JUGÁ Y DESCUBRÍ TODAS</h2>
-						<h1>NUESTRAS VARIEDADES</h1>
-					</div>
-					<div className="cards">
-						{cards.map((card, i) => (
-							<Card value={card} key={i} onFlip={handleFlip(i)} flipped={selection.includes(i)} color={(i < 14 ? i % 3 : (i + 1) % 3) as 1 | 2 | 3} />
-						))}
-					</div>
-					{Boolean(infoText) && <div className="infoText">{infoText}</div>}
-				</div>
-			) : page === 3 ? (
-				<div className="description">
-					{winningCard ? (
-						<>
-							<h3>Nuestros Vinos</h3>
-							<h1>{texts[winningCard].category}</h1>
-							<h2>{texts[winningCard].variety}</h2>
-							{texts[winningCard].description.map((p) => (
-								<p>{p}</p>
+		<div className="container">
+			<div className={`container2 ${window.location.hash === '#flip' ? 'flip' : ''}`}>
+				{page === 2 ? (
+					<div className="game">
+						<div className="header">
+							<h2>JUGÁ Y DESCUBRÍ TODAS</h2>
+							<h1>NUESTRAS VARIEDADES</h1>
+						</div>
+						<div className="cards">
+							{cards.map((card, i) => (
+								<Card value={card} key={i} onFlip={handleFlip(i)} flipped={selection.includes(i)} color={(i < 14 ? i % 3 : (i + 1) % 3) as 1 | 2 | 3} />
 							))}
-						</>
-					) : (
-						<>
-							<h3>Oh… perdiste. </h3>
-							<h2>Muchas gracias por participar y esperamos que sigas disfrutando de nuestros vinos.</h2>
-						</>
-					)}
-					<div className="buttonsWrapper">
-						<div className="buttons">
-							<div className="button buttonPlay" onClick={() => playGame()}>
-								Volver a jugar
-							</div>
-							<div className="button buttonPlay" onClick={() => nextPage(1)}>
-								Ver video
+						</div>
+						{Boolean(infoText) && <div className="infoText">{infoText}</div>}
+					</div>
+				) : page === 3 ? (
+					<div className="description">
+						{winningCard ? (
+							<>
+								<h3>Nuestros Vinos</h3>
+								<div className="wineName">
+									<h1>{texts[winningCard].category}</h1>
+									<h2>{texts[winningCard].variety}</h2>
+								</div>
+								<img src={cardImageBig[winningCard - 1]} />
+								{texts[winningCard].description.map((p) => (
+									<p>{p}</p>
+								))}
+							</>
+						) : (
+							<>
+								<h1>Oh… perdiste.</h1>
+								<h1>&nbsp;</h1>
+								<p>
+									Muchas gracias por participar. <br />
+									Esperamos que sigas disfrutando de nuestros vinos.
+								</p>
+								<h1>&nbsp;</h1>
+							</>
+						)}
+						<div className="buttonsWrapper">
+							<div className="buttons">
+								<div className="button buttonPlay" onClick={() => playGame()}>
+									Jugar
+								</div>
+								<div className="button buttonPlay" onClick={() => nextPage(1)}>
+									Video
+								</div>
 							</div>
 						</div>
 					</div>
-				</div>
-			) : (
-				<div className="video" onClick={() => playGame()}>
-					<video width="1920" height="1080" autoPlay muted>
-						<source src={video} type="video/mp4" />
-					</video>
-					<div className="buttonsWrapper">
-						<div className="button buttonPlay">Jugar</div>
+				) : (
+					<div className="video" onClick={() => playGame()}>
+						<video width="1920" height="1080" autoPlay muted>
+							<source src={video} type="video/mp4" />
+						</video>
+						<div className="buttonsWrapper">
+							<div className="button buttonPlay">Jugar</div>
+						</div>
 					</div>
-				</div>
-			)}
-			<div className="background" style={{ backgroundImage: `url(${bg})` }}></div>
-		</>
+				)}
+				<div className="background" style={{ backgroundImage: `url(${bg})` }}></div>
+			</div>
+		</div>
 	);
 }
 interface CardProps {
@@ -154,7 +167,6 @@ interface CardProps {
 	color: 1 | 2 | 3;
 }
 function Card({ value, onFlip, flipped, color }: CardProps) {
-	const cardImage = [card1, card2, card3, card4, card5];
 	return (
 		<div className={`card ${flipped ? 'flipped' : ''}`} onClick={onFlip}>
 			<div className="card-body">
